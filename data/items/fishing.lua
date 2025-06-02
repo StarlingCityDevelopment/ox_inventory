782272ef07d647e215ce7206c3ada8d316d1f407
Codexis <contact@codexis.dev>
2025-06-02 08:06:12 +0200

    feat(items): add fishing items

---
+++ data/items/fishing.lua
@@ -0,0 +1,79 @@
+return {
+    ['basic_rod'] = {
+        label = 'Canne à pêche simple',
+        stack = false,
+        weight = 250
+    },
+
+    ['graphite_rod'] = {
+        label = 'Canne à pêche en graphite',
+        stack = false,
+        weight = 350
+    },
+
+    ['titanium_rod'] = {
+        label = 'Canne à pêche en titane',
+        stack = false,
+        weight = 450
+    },
+
+    ['worms'] = {
+        label = 'Vers',
+        weight = 10
+    },
+
+    ['artificial_bait'] = {
+        label = 'Appât artificiel',
+        weight = 30
+    },
+
+    ['anchovy'] = {
+        label = 'Anchois',
+        weight = 20
+    },
+
+    ['grouper'] = {
+        label = 'Mérou',
+        weight = 3500
+    },
+
+    ['haddock'] = {
+        label = 'Aiglefin',
+        weight = 500
+    },
+
+    ['mahi_mahi'] = {
+        label = 'Mahi Mahi',
+        weight = 3500
+    },
+
+    ['piranha'] = {
+        label = 'Piranha',
+        weight = 1500
+    },
+
+    ['red_snapper'] = {
+        label = 'Vivaneau rouge',
+        weight = 2500
+    },
+
+    ['salmon'] = {
+        label = 'Saumon',
+        weight = 1000
+    },
+
+    ['shark'] = {
+        label = 'Requin',
+        weight = 7500
+    },
+
+    ['trout'] = {
+        label = 'Truite',
+        weight = 750
+    },
+
+    ['tuna'] = {
+        label = 'Thon',
+        weight = 10000
+    },
+}
