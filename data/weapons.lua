0d5a966eb89968a064f53acefc3a9732d380badb
Codexis <contact@codexis.dev>
2025-08-31 10:29:19 +0200

    feat(items): new weapons

--- data/weapons.lua
+++ data/weapons.lua
@@ -180,6 +180,393 @@ return {
 			ammoname = 'ammo-9'
 		},
 
+		-- Kyros Weapon Pack V4
+
+        ['WEAPON_GLOCK19X'] = {
+            label = 'G19X XL',
+            weight = 2800,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GLOCK19'] = {
+            label = 'GLOCK 19',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GLOCK40'] = {
+            label = 'GLOCK 40',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GLOCK40S'] = {
+            label = 'GLOCK 40 SWITCH',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_DDM4V5'] = {
+            label = 'DDM4V5',
+            weight = 6400,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_MPX'] = {
+            label = 'MPX',
+            weight = 6600,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_PD509'] = {
+            label = 'PD 509',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_APS'] = {
+            label = 'STECHKIN APS',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_PPK'] = {
+            label = 'WALTHER PPK',
+            weight = 2500,
+            ammoname = 'ammo-22'
+        },
+
+		['WEAPON_PITVIPER'] = {
+            label = 'JW4 PIT VIPER',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_BARP'] = {
+            label = 'BANSHEE ARP-9',
+            weight = 4500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_RDP'] = {
+            label = 'HELLCAT RDP',
+            weight = 2400,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_LOK'] = {
+            label = 'M-LOK AR 15',
+            weight = 6000,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_PLR'] = {
+            label = 'PLR-16',
+            weight = 3400,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_XDS9'] = {
+            label = 'XDS9',
+            weight = 2800,
+            ammoname = 'ammo-45'
+        },
+
+		['WEAPON_PPS'] = {
+            label = 'WALTHER PPS',
+            weight = 2400,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_XDME'] = {
+            label = 'XDM ELITE',
+            weight = 1800,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_SP'] = {
+            label = 'FN-15 PISTOL',
+            weight = 4200,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_GLOCK17S'] = {
+            label = 'GLOCK 17 SWITCH',
+            weight = 2300,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GLOCK18C'] = {
+            label = 'GLOCK 18C',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GLOCK19S'] = {
+            label = 'GLOCK 19 SWITCH',
+            weight = 2300,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_ARPISTOL'] = {
+            label = 'TATIC ARP 9MM',
+            weight = 5500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GLOCK26S'] = {
+            label = 'GLOCK 26 SWITCH',
+            weight = 2500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GRENADES'] = {
+            label = 'GRENADE SNIPER',
+            weight = 15000,
+            ammoname = 'ammo-grenade'
+        },
+
+		['WEAPON_M32S'] = {
+            label = 'M32 SMOKE GL',
+            weight = 8000,
+            ammoname = 'ammo-smokegrenade'
+        },
+
+		['WEAPON_MICRODRACO'] = {
+            label = 'MICRO DRACO',
+            weight = 5600,
+            ammoname = 'ammo-rifle2'
+        },
+
+		['WEAPON_RUGER57'] = {
+            label = 'RUGER 57',
+            weight = 2200,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_MK18'] = {
+            label = 'MK18 MOD 1',
+            weight = 5200,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_680'] = {
+            label = 'MODEL 680',
+            weight = 6200,
+            ammoname = 'ammo-shotgun'
+        },
+
+		['WEAPON_ZIPTIE'] = {
+            label = 'ZIP TIE M4A1',
+            weight = 8500,
+            ammoname = 'ammo-rifle'
+        },
+
+		-- Kyros Weapon Pack V3
+
+        ['WEAPON_DDM4V7'] = {
+			label = 'DDM4V7 AR15',
+			weight = 6500,
+			ammoname = 'ammo-rifle'
+		},
+
+		['WEAPON_G2C'] = {
+			label = 'TAURUS G2C',
+			weight = 6500,
+			ammoname = 'ammo-9'
+		},
+
+		['WEAPON_PMR'] = {
+			label = 'KEL-TEC-PMR',
+			weight = 6500,
+			ammoname = 'ammo-9'
+		},
+
+		['WEAPON_G19G4'] = {
+            label = 'G19G4',
+            weight = 5000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_GS'] = {
+            label = 'GLOCK STAINLESS',
+            weight = 5000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_SP320'] = {
+            label = 'SP320',
+            weight = 5000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_BP'] = {
+            label = 'Beretta Pico',
+            weight = 5000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_TGLOCK'] = {
+            label = 'TAN GLOCK',
+            weight = 5000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_G19X'] = {
+            label = 'GLOCK 19X',
+            weight = 5000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_AWP'] = {
+            label = 'AWP',
+            weight = 15000,
+            ammoname = 'ammo-sniper'
+        },
+
+		['WEAPON_TUZI'] = {
+            label = 'TACTICAL UZI',
+            weight = 5000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_BROOM'] = {
+			label = 'WOOD BROOM',
+			weight = 1000,
+		},
+
+		['WEAPON_TOMAHAWK'] = {
+			label = 'TOMAHAWK',
+			weight = 1000,
+		},
+
+		['WEAPON_TRIDAGGER'] = {
+			label = 'TRI DAGGER',
+			weight = 1000,
+		},
+
+		['WEAPON_SHAMMER'] = {
+			label = 'Sledge Hammer',
+			weight = 3000,
+		},
+
+		['WEAPON_DILDO'] = {
+			label = 'Dildo',
+			weight = 1300,
+		},
+
+		['WEAPON_UK'] = {
+			label = 'UTILITY KNIFE',
+			weight = 500,
+		},
+
+		['WEAPON_SD'] = {
+			label = 'SCREW DRIVER',
+			weight = 650,
+		},
+
+		['WEAPON_M200'] = {
+            label = 'CheyTac M200',
+            weight = 10000,
+            ammoname = 'ammo-sniper'
+        },
+
+		['WEAPON_SCAR'] = {
+            label = 'SCAR',
+            weight = 6500,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_552'] = {
+            label = 'SG-552',
+            weight = 5000,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_L1A1'] = {
+            label = 'FN L1A1',
+            weight = 5000,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_FAL'] = {
+            label = 'FN FAL',
+            weight = 5000,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_MCX'] = {
+            label = 'SIG MCX',
+            weight = 5000,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_P2011'] = {
+            label = 'P2011',
+            weight = 3000,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_P416'] = {
+            label = 'P416',
+            weight = 7500,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_HIPOINT'] = {
+            label = 'HI POINT',
+            weight = 3500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_DE'] = {
+            label = 'DESERT EAGLE',
+            weight = 3500,
+            ammoname = 'ammo-50'
+        },
+
+		['WEAPON_AUGA1'] = {
+            label = 'AUG A1',
+            weight = 5500,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_P90'] = {
+            label = 'P90',
+            weight = 5500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_VESPER'] = {
+            label = 'VESPER',
+            weight = 5500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_H12'] = {
+            label = 'HAYMAKER 12',
+            weight = 6500,
+            ammoname = 'ammo-shotgun'
+        },
+
+		['WEAPON_G26'] = {
+            label = 'GLOCK 26',
+            weight = 3500,
+            ammoname = 'ammo-9'
+        },
+
+		['WEAPON_IA2'] = {
+            label = 'IA2',
+            weight = 4500,
+            ammoname = 'ammo-rifle'
+        },
+
+		['WEAPON_AS50'] = {
+            label = 'AS50',
+            weight = 10500,
+            ammoname = 'ammo-heavysniper'
+        },
+
 		-- Kyros Weapon Pack V2
 
 		['WEAPON_M32'] = {
@@ -2408,6 +2795,11 @@ return {
 		['ammo_paintball'] = {
 			label = 'Paint Ball',
 			weight = 2,
+		},
+
+        ['ammo-smokegrenade'] = {
+			label = '40mm Smoke',
+			weight = 3,
 		},
 	}
 }
