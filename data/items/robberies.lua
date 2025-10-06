c68641d8b9affcde326d93fde963ad01b5d49d72
Codexis <contact@codexis.dev>
2025-10-06 10:46:21 +0200

    feat(data/items): added house robbery

--- data/items/robberies.lua
+++ data/items/robberies.lua
@@ -30,4 +30,235 @@ return {
         label = "Corde",
         weight = 1750,
     },
+
+    ['hack_laptop'] = {
+        label = 'Hacking Laptop',
+        weight = 1250,
+        stack = true
+    },
+
+    ['loot_bag'] = {
+        label = 'Duffle bag',
+        weight = 800,
+        stack = true
+    },
+
+    ['printer'] = {
+        label = 'Printer',
+        weight = 2750,
+        stack = true
+    },
+
+    ['npc_phone'] = {
+        label = 'Phone',
+        weight = 150,
+        stack = true
+    },
+
+    ['monitor'] = {
+        label = 'Monitor',
+        weight = 1500,
+        stack = true
+    },
+
+    ['television'] = {
+        label = 'TV',
+        weight = 3000,
+        stack = true
+    },
+
+    ['flat_television'] = {
+        label = 'Flat TV',
+        weight = 3000,
+        stack = true
+    },
+
+    ['radio_alarm'] = {
+        label = 'Radio',
+        weight = 500,
+        stack = true
+    },
+
+    ['fan'] = {
+        label = 'Fan',
+        weight = 1500,
+        stack = true
+    },
+
+    ['lockpick'] = {
+        label = 'Lockpick',
+        description = "Can lockpick any doors if you have enough skill!",
+        weight = 50,
+        stack = true
+    },
+
+    ['shoebox'] = {
+        label = 'Shoe box',
+        weight = 200,
+        stack = true
+    },
+
+    ['dj_deck'] = {
+        label = 'DJ Deck',
+        weight = 1000,
+        stack = true
+    },
+
+    ['console'] = {
+        label = 'Console',
+        weight = 500,
+        stack = true
+    },
+
+    ['boombox'] = {
+        label = 'Boombox',
+        weight = 2000,
+        stack = true
+    },
+
+    ['bong'] = {
+        label = 'Bong',
+        weight = 300,
+        stack = true
+    },
+
+    ['coffemachine'] = {
+        label = 'Coffe machine',
+        weight = 1500,
+        stack = true
+    },
+
+    ['tapeplayer'] = {
+        label = 'Tape Player',
+        weight = 1000,
+        stack = true
+    },
+
+    ['hairdryer'] = {
+        label = 'Hairdryer',
+        weight = 500,
+        stack = true
+    },
+
+    ['j_phone'] = {
+        label = 'Phone',
+        weight = 150,
+        stack = true
+    },
+
+    ['sculpture'] = {
+        label = 'Sculpture',
+        weight = 2000,
+        stack = true
+    },
+
+    ['toiletry'] = {
+        label = 'Toiletry',
+        weight = 100,
+        stack = true
+    },
+
+    ['pogo'] = {
+        label = 'Art Piece',
+        description = "Pogo Statue",
+        weight = 1000,
+        stack = true
+    },
+
+    ['powder'] = {
+        label = 'Bag with powder',
+        description = "Good for discovering lasers that are not visible",
+        weight = 100,
+        stack = true
+    },
+
+    ['bracelet'] = {
+        label = 'Bracelet',
+        weight = 20,
+        stack = true
+    },
+
+    ['book'] = {
+        label = 'Book',
+        weight = 300,
+        stack = true
+    },
+
+    ['earings'] = {
+        label = 'Earings',
+        weight = 5,
+        stack = true
+    },
+
+    ['gold_bracelet'] = {
+        label = 'Gold bracelet',
+        weight = 50,
+        stack = true
+    },
+
+    ['gold_watch'] = {
+        label = 'Gold watch',
+        weight = 100,
+        stack = true
+    },
+
+    ['house_locator'] = {
+        label = 'House locator',
+        weight = 200,
+        stack = true
+    },
+
+    ['necklace'] = {
+        label = 'Necklace',
+        weight = 30,
+        stack = true
+    },
+
+    ['notepad'] = {
+        label = 'Notepad',
+        weight = 50,
+        stack = true
+    },
+
+    ['pencil'] = {
+        label = 'Pencil',
+        weight = 10,
+        stack = true
+    },
+
+    ['romantic_book'] = {
+        label = 'Romantic book',
+        weight = 300,
+        stack = true
+    },
+
+    ['shampoo'] = {
+        label = 'Shampoo',
+        weight = 300,
+        stack = true
+    },
+
+    ['soap'] = {
+        label = 'Soap',
+        weight = 100,
+        stack = true
+    },
+
+    ['toothpaste'] = {
+        label = 'Toothpaste',
+        weight = 50,
+        stack = true
+    },
+
+    ['watch'] = {
+        label = 'Watch',
+        weight = 50,
+        stack = true
+    },
+
+    ['skull'] = {
+        label = 'Skull Art with diamonds',
+        weight = 500,
+        stack = true
+    },
 }
