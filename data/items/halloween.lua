f4170e92948dd526256918c4a0ea9e0c581c07a2
LeKaleidoscope <raphael.writers@gmail.com>
2025-10-22 15:52:09 +0200

    fix(data/items): removed useless halloween items

--- data/items/halloween.lua
+++ data/items/halloween.lua
@@ -1,17 +1,6 @@
 return {
     ['event_token'] = {
         label = 'Event Token',
-        weight = 1,
-    },
-
-    ['candy_corn'] = {
-        label = 'Candy Corn',
-        weight = 1,
-    },
-
-    ['chocolate_bar'] = {
-        label = 'Chocolate Bar',
-        weight = 1,
     },
 
     ['pumpkin_basket'] = {
