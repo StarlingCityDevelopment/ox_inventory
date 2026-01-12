5be97a32d8a2d0aaf002edb954127faa4795d141
Codexis <contact@codexis.dev>
2026-01-12 08:36:56 +0100

    feat(items): add cleaning kit

--- data/items/mechanics.lua
+++ data/items/mechanics.lua
@@ -5,35 +5,35 @@ return {
         label = "Porte de véhicule",
         weight = 2500, -- 15000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_vehiclehood"] = {
         label = "Capot de véhicule",
         weight = 2000, -- 12000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_vehicletrunk"] = {
         label = "Coffre de véhicule",
         weight = 3000, -- 10000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_vehiclewheel"] = {
         label = "Roue de véhicule",
         weight = 4000, -- 10000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_vehiclewindow"] = {
         label = "Vitre de véhicule",
         weight = 1250, -- 2500g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     -- Pièces principales
@@ -42,56 +42,56 @@ return {
         label = "Alternateur",
         weight = 3000, -- 6000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_brakes"] = {
         label = "Freins",
         weight = 3500, -- 7000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_electricmotor"] = {
         label = "Moteur électrique",
         weight = 10000, -- 40000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_evbattery"] = {
         label = "Batterie VE",
         weight = 5000, -- 300000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_fuelinjector"] = {
         label = "Injecteur de carburant",
         weight = 100, -- 200g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_powersteeringpump"] = {
         label = "Pompe de direction assistée",
         weight = 1250, -- 2500g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_radiator"] = {
         label = "Radiateur",
         weight = 5000, -- 6000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_transmission"] = {
         label = "Transmission",
         weight = 4000, -- 80000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     -- Pièces d'entretien
@@ -100,91 +100,91 @@ return {
         label = "Filtre à air",
         weight = 50, -- 300g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_batterycoolant"] = {
         label = "Liquide de refroidissement de batterie",
         weight = 75, -- 1500g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_brakefluid"] = {
         label = "Liquide de frein",
         weight = 200, -- 1000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_brakepad"] = {
         label = "Plaquettes de frein",
         weight = 1000, -- 2000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_coolant"] = {
         label = "Liquide de refroidissement",
         weight = 100, -- 2000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_drivebelt"] = {
         label = "Courroie d'entraînement",
         weight = 400, -- 800g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_fuelfilter"] = {
         label = "Filtre à carburant",
         weight = 150, -- 600g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_hvwiring"] = {
         label = "Câblage haute tension",
         weight = 300, -- 3000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_oilfilter"] = {
         label = "Filtre à huile",
         weight = 100, -- 900g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_sparkplugs"] = {
         label = "Bougies d'allumage",
         weight = 100, -- 400g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_steeringfluid"] = {
         label = "Liquide de direction assistée",
         weight = 50, -- 1000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_tires"] = {
         label = "Pneus",
         weight = 2000, -- set / 40000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_transmissionfluid"] = {
         label = "Liquide de transmission",
         weight = 125, -- 2500g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     -- Kits
@@ -193,63 +193,63 @@ return {
         label = "Kit de réparation",
         weight = 500, -- 2000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_repairkit_adv"] = {
         label = "Kit de réparation avancé",
         weight = 1000, -- 5000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_carjack"] = {
         label = "Cric",
         weight = 1000, -- 5000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_patchkit"] = {
         label = "Kit de rustines",
         weight = 200, -- 400g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_fuelcan"] = {
         label = "Jerrican",
         weight = 500, -- 2000g / 2 (empty)
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_jumpstarter"] = {
         label = "Démarreur portable",
         weight = 600, -- 5000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_repairkit_tire"] = {
         label = "Kit de réparation de pneu",
         weight = 400, -- 800g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_diagnostictool"] = {
         label = "Outil de diagnostic véhicule",
         weight = 75, -- 1500g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_servicebook"] = {
         label = "Carnet d'entretien",
         weight = 10, -- 300g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     -- Objets d'animation
@@ -258,34 +258,47 @@ return {
         label = "Cône de signalisation",
         weight = 100, -- 3000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_toolstrolley"] = {
         label = "Chariot d'outils",
         weight = 5000, -- 15000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_toolbox"] = {
         label = "Boîte à outils",
         weight = 250, -- 5000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_consign"] = {
         label = "Panneau de chantier",
         weight = 150, -- 3000g / 2
         stack = true,
-        close = true
+        close = true,
     },
 
     ["t1ger_roadbarrier"] = {
         label = "Barrière routière",
         weight = 1000, -- 20000g / 2
         stack = true,
-        close = true
+        close = true,
+    },
+
+    -- Divers
+
+    ["cleaningkit"] = {
+        label = "Kit de Nettoyage",
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "Un chiffon microfibre avec du savon rendra votre voiture étincelante à nouveau !",
+        server = {
+            export = "vehiclehandler.cleaningkit",
+        },
     },
 }
