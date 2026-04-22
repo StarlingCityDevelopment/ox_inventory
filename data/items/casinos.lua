ad7882fa5fd3ea149508337d8615a07844335846
Codexis <contact@codexis.dev>
2026-04-22 09:00:55 +0200

    feat(data): added casinos items

---
+++ data/items/casinos.lua
@@ -0,0 +1,57 @@
+return {
+    ["blackjack_table"] = {
+        label = "Table de Blackjack",
+        weight = 1,
+        stack = true,
+        close = true,
+        description = nil,
+    },
+
+    ["baccarat_table"] = {
+        label = "Table de Baccarat",
+        weight = 1,
+        stack = true,
+        close = true,
+        description = nil,
+    },
+
+    ["poker_table"] = {
+        label = "Table de Poker",
+        weight = 1,
+        stack = true,
+        close = true,
+        description = nil,
+    },
+
+    ["roulette_table"] = {
+        label = "Table de Roulette",
+        weight = 1,
+        stack = true,
+        close = true,
+        description = nil,
+    },
+
+    ["wheel_machine"] = {
+        label = "Roue de la Fortune",
+        weight = 1,
+        stack = true,
+        close = true,
+        description = nil,
+    },
+
+    ["slot_machine"] = {
+        label = "Machine à Sous",
+        weight = 1,
+        stack = true,
+        close = true,
+        description = nil,
+    },
+
+    ["horseracing_machine"] = {
+        label = "Machine de Course Hippique",
+        weight = 1,
+        stack = true,
+        close = true,
+        description = nil,
+    },
+}
