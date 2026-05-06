0ad44b10b4e8e593d9786ba4626eabb6c185fa1c
Codexis <contact@codexis.dev>
2026-05-06 13:46:51 +0200

    fix(data): removed useless exports from clothes items

--- data/items/clothes.lua
+++ data/items/clothes.lua
@@ -4,9 +4,6 @@ return {
         description = 'Une veste élégante pour se protéger du froid et compléter votre style',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_legs'] = {
@@ -14,9 +11,6 @@ return {
         description = 'Un pantalon confortable et pratique pour toutes les occasions',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_bags'] = {
@@ -24,9 +18,6 @@ return {
         description = 'Un sac à dos pratique pour transporter vos affaires personnelles',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_torsos'] = {
@@ -34,9 +25,6 @@ return {
         description = 'Des gants pour protéger vos mains et améliorer votre grip',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_shoes'] = {
@@ -44,9 +32,6 @@ return {
         description = 'Des chaussures confortables pour marcher en toute sécurité',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_shirts'] = {
@@ -54,9 +39,6 @@ return {
         description = 'Une chemise classique pour un look professionnel et soigné',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_vest'] = {
@@ -64,9 +46,6 @@ return {
         description = 'Un gilet pare-balle pour une protection contre les projectiles',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_decals'] = {
@@ -74,9 +53,6 @@ return {
         description = 'Un badge officiel pour identifier votre rang ou profession',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_neck'] = {
@@ -84,9 +60,6 @@ return {
         description = 'Une chaîne décorative pour accessoiriser votre cou',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_hats'] = {
@@ -94,9 +67,6 @@ return {
         description = 'Un chapeau pour vous protéger du soleil et compléter votre look',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_glasses'] = {
@@ -104,9 +74,6 @@ return {
         description = 'Des lunettes de soleil pour protéger vos yeux et avoir du style',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_earrings'] = {
@@ -114,9 +81,6 @@ return {
         description = 'Des boucles d\'oreilles élégantes pour sublimer votre visage',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_watches'] = {
@@ -124,9 +88,6 @@ return {
         description = 'Une montre pour garder un œil sur l\'heure avec style',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_bracelets'] = {
@@ -134,9 +95,6 @@ return {
         description = 'Un bracelet décoratif pour orner votre poignet',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_masks'] = {
@@ -144,9 +102,6 @@ return {
         description = 'Un masque pour dissimuler votre identité ou vous protéger',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 
     ['clothes_outfits'] = {
@@ -154,8 +109,5 @@ return {
         description = 'Une tenue complète pour un look parfaitement coordonné',
         weight = 0,
         stack = false,
-        client = {
-            export = 'ox_inventory.useClothingItem'
-        }
     },
 }
